7814fa98e46ce9dc4ffecf4406b24444dfb1b892
SHB <shahabnikkhoo@gmail.com>
2021-09-03 01:11:08 +0430

    python code test

--- HiveOs_api/Temp_Fan_read.py
+++ HiveOs_api/Temp_Fan_read.py
@@ -1,7 +1,17 @@
 from requests import request, exceptions
 from time import sleep
 import json
+import serial
+import serial.tools.list_ports
 import SecretData as sd
+import time
+
+
+
+fan_serial = None
+fans_flag = [0,0,0,0,0,0,0,0,0,0,0,0]
+
+
 
 
 class Hive(object):
@@ -53,15 +63,66 @@ class Hive(object):
         return self.api_query('PATCH', '/farms/' + farm_id, params)
 
 
-def main():
 
+
+def Search_for_fan_serial_port():
+    global fan_serial
+    available_ports = [tuple(p) for p in list(serial.tools.list_ports.comports())]
+    for port in available_ports:
+        test_serial = serial.Serial(
+            port = port[0],
+            baudrate = 9600
+        )
+        test_serial.close()
+        test_serial.open()
+        time_last = time.time()
+        while True:
+            if test_serial.inWaiting():
+                text = test_serial.readline()#.decode('utf-8')
+                if text.find('Arduino ON') != -1:
+                    fan_serial = test_serial
+                    print('!!!arduino port found on port %s!!!'%port[0])
+                    break
+                else:pass
+
+
+            if time.time()-time_last > 10:
+                print('!!!! this port is not arduino %s  !!!!'%port[0])
+                break
+
+
+
+
+
+def check_temp(fan,temp):
+    global fans_flag,fan_serial
+    for index,fan_value in enumerate(fan):
+        if not fans_flag[index]:
+            if fan_value > 60:
+                fan_serial.write('%d,1\n'%index)
+                fans_flag[index] = 1
+        else:
+            if fan_value < 43:
+                fan_serial.write('%d,0\n'%index)
+                fans_flag[index] = 1
+        
+
+
+def main():
     cHive = Hive(sd.get_token())
-    data = cHive.get_worker_info(sd.get_farm_id(),sd.get_worker_id())["miners_stats"]["hashrates"][0]
-    fan_perc = data['fans']
-    graphic_temp = data['temps']
-    print(fan_perc)
-    print(graphic_temp) 
+    while True:
+        if time.time - last_time > 180:
+            last_time = time.time
+            try:
+                data = cHive.get_worker_info(sd.get_farm_id(),sd.get_worker_id())["miners_stats"]["hashrates"][0]
+                fan_perc = data['fans']
+                graphic_temp = data['temps']
+                print(fan_perc,graphic_temp)
+                check_temp(fan_perc,graphic_temp)
+            except Exception as e:
+                print('error in main loop',e) 
 
 
 if __name__ == '__main__':
+    Search_for_fan_serial_port()
     main()
